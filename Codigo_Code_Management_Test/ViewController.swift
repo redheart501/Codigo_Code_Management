1c23ae64d743676581927b550f693b30f193cfd2
Kyaw Ye Htun <redheart501@gmail.com>
2023-10-24 17:24:08 +0630

    Initial Commit

---
+++ Codigo_Code_Management_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Codigo_Code_Management_Test
+//
+//  Created by Kyaw Ye Htun on 24/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
